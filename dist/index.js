6c298ff82e1eadbd568501aac9fe225c03208515
lauracavaliere <68373291+lauracavaliere@users.noreply.github.com>
2021-08-16 11:14:13 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1235,18 +1235,9 @@ async function run() {
       bodyUppercaseBaseMatch: (core.getInput('body-uppercase-base-match').toLowerCase() === 'true'),
       bodyUppercaseHeadMatch: (core.getInput('body-uppercase-head-match').toLowerCase() === 'true'),
     }
-
-    console.log("input context:", inputs.context);
-    if (inputs.context != "") {
-      const prContext = JSON.parse(inputs.context);
-      console.log("pr context base ref:",  prContext.base.ref);  
-    }
-    else {
-      const prContext = github.context.payload.pull_request;
-      console.log("pr context head ref:",  prContext.head.ref);
-    }
-    console.log("pr context:", prContext);
-	  
+    
+    const prContext = (inputs.context != "") ? JSON.parse(inputs.context) : github.context.payload.pull_request;
+     
     const baseBranchRegex = inputs.baseBranchRegex.trim();
     const matchBaseBranch = baseBranchRegex.length > 0;
 
