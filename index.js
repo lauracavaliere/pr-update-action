dd85cf93922f951b9b2370a551fd398b15fedb53
lauracavaliere <68373291+lauracavaliere@users.noreply.github.com>
2021-08-16 10:07:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ async function run() {
     const headTokenRegex = new RegExp('%headbranch%', "g");
 
     const inputs = {
+      context: core.getInput('pr-context', {required: false}),
       token: core.getInput('repo-token', {required: true}),
       baseBranchRegex: core.getInput('base-branch-regex'),
       headBranchRegex: core.getInput('head-branch-regex'),
@@ -23,6 +24,8 @@ async function run() {
       bodyUppercaseHeadMatch: (core.getInput('body-uppercase-head-match').toLowerCase() === 'true'),
     }
 
+    const prContext = inputs.context ? github.context.payload.pull_request : inputs.context;
+    
     const baseBranchRegex = inputs.baseBranchRegex.trim();
     const matchBaseBranch = baseBranchRegex.length > 0;
 
@@ -40,7 +43,7 @@ async function run() {
     }
 
     if (matchBaseBranch) {
-      const baseBranchName = github.context.payload.pull_request.base.ref;
+      const baseBranchName = prContext.base.ref;
       const baseBranch = inputs.lowercaseBranch ? baseBranchName.toLowerCase() : baseBranchName;
       core.info(`Base branch: ${baseBranch}`);
 
@@ -57,7 +60,7 @@ async function run() {
     }
 
     if (matchHeadBranch) {
-      const headBranchName = github.context.payload.pull_request.head.ref;
+      const headBranchName =  prContext.head.ref;
       const headBranch = inputs.lowercaseBranch ? headBranchName.toLowerCase() : headBranchName;
       core.info(`Head branch: ${headBranch}`);
 
@@ -76,12 +79,12 @@ async function run() {
     const request = {
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      pull_number: github.context.payload.pull_request.number,
+      pull_number:  prContext.number,
     }
 
     const upperCase = (upperCase, text) => upperCase ? text.toUpperCase() : text;
 
-    const title = github.context.payload.pull_request.title || '';
+    const title = prContext.title || '';
     const processedTitleText = inputs.titleTemplate
       .replace(baseTokenRegex, upperCase(inputs.titleUppercaseBaseMatch, matches.baseMatch))
       .replace(headTokenRegex, upperCase(inputs.titleUppercaseHeadMatch, matches.headMatch));
@@ -106,7 +109,7 @@ async function run() {
       core.warning('No updates were made to PR title');
     }
 
-    const body = github.context.payload.pull_request.body || '';
+    const body =  prContext.body || '';
     const processedBodyText = inputs.bodyTemplate
       .replace(baseTokenRegex, upperCase(inputs.bodyUppercaseBaseMatch, matches.baseMatch))
       .replace(headTokenRegex, upperCase(inputs.bodyUppercaseHeadMatch, matches.headMatch));
